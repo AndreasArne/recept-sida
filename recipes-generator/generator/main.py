0aebc5e262bc81541047f5c17db1a3108b01aafc
Andreas Arnesson <andreas_osby@hotmail.com>
2019-01-14 21:41:13 +0100

    started on static site generator

---
+++ recipes-generator/generator/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Generate html-files for recipes
+"""
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+file_loader = FileSystemLoader("templates")
+env = Environment(autoescape=select_autoescape(
+        disabled_extensions=('txt',),
+        default_for_string=True,
+        default=True),
+    loader=file_loader)
+
+
+template = env.get_template("recipe.html")
+
+output = template.render()
+print(output)
+
+if __name__ == "__main__":
+    
